d78ef763691c3317dd769d02fed2a6cd5a5b4050
Markolatie <markolalo@hotmail.com>
2023-06-16 23:33:57 +0300

    edit on fibonacci series program
    cleaned the program by removing unnecessary print() functions

--- fibonacci.py
+++ fibonacci.py
@@ -14,8 +14,8 @@ The sequence begins with 0 and 1, and the next number is the
 
 while True: # Main program loop
     while True: #Keep asking until the user enters valid input
-        print('Enter the Nth Fibonacci number you wish to')
-        print('Calculate (such as 5, 50, 1000, 9999), or QUIT to quit!')
+        print('Enter the Nth Fibonacci number you wish to\n'
+              'Calculate (such as 5, 50, 1000, 9999), or QUIT to quit!')
         response = input('>').upper()
 
         if response == 'QUIT':
@@ -26,14 +26,12 @@ while True: # Main program loop
             nth = int(response)
             break
 
-        print('Please enter a number greater than 0, or QUIT.')
+        print('Please enter a number greater than 0, or QUIT.\n\n')
     print()
 
     # Handle the special cases if the user entered 1 or 2:
     if nth == 1:
-        print('0')
-        print()
-        print('The #1 Fibonacci number is 0.')
+        print('0\nThe #1 Fibonacci number is 0')
         continue
     elif nth == 2:
         print('0,1')
@@ -43,9 +41,9 @@ while True: # Main program loop
 
     # Display warning if the user entered a large number:
     if nth >= 10000:
-        print('WARNING: This will take a while to display on the')
-        print('screen. If you want to quit this before it is0')
-        print('done, press Ctrl-C')
+        print('WARNING: This will take a while to display on the\n'
+              'screen. If you want to quit this before it is\n'
+              'done, press Ctrl-C')
         input('Press Enter to begin....')
 
     # Calculate the Nth Fibonacci number:
